50cd8da8ca7b29fccef45a8217cc45ea832f3fa8
sivadarsan <sivadarsan48@gmail.com>
2026-01-31 19:23:43 +0530

    missing comma in jwt-secret-key at main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     SessionMiddleware,
-    secret_key=settings.JWT_SECRET_KEY
+    secret_key=settings.JWT_SECRET_KEY,
     same_site="none",
     https_only=True
 )
